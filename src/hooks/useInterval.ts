f9bc02c0041953088e3142bbce34a53a85a46288
dilaouid <dilaouid@student.42.fr>
2024-02-22 13:00:51 +0100

    (+) useInterval hook

---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from 'react';
+
+function useInterval(callback: () => void, delay: number | null) {
+  const savedCallback = useRef<() => void>();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      if (typeof savedCallback?.current !== 'undefined') {
+        savedCallback.current();
+      }
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+export default useInterval;
