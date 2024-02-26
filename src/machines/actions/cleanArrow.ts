ff055a0b1faf01046424a6150347c1398d9d4e1d
dilaouid <dilaouid@student.42.fr>
2024-02-26 22:50:53 +0100

    (+) clean arrow  action file

---
+++ src/machines/actions/cleanArrow.ts
@@ -0,0 +1,10 @@
+import { GameContext, GameEvent } from "../types";
+
+export const cleanArrowAssignGrid = (context: GameContext, event: GameEvent) => {
+    if (event.type !== 'CLEAN_ARROW') return context.grid;
+    const newGrid = context.grid.map(row => [...row]);
+    if (!event.position || event.position?.y < 0 || event.position?.y >= newGrid.length || event.position?.x < 0 || event.position?.x >= newGrid[0].length)
+        return newGrid;
+    newGrid[event.position.y][event.position.x] = 'back';
+    return newGrid;
+};
