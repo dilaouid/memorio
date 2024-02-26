f72ecf21c6730c1480e188ae021c8a842a9f08f0
dilaouid <dilaouid@student.42.fr>
2024-02-26 22:57:46 +0100

    (+) remove score popup action file

---
+++ src/machines/actions/removeScorePopup.ts
@@ -0,0 +1,8 @@
+import { GameContext } from "../types";
+
+export const removeScorePopupAssign = (context: GameContext) => {
+    const lastPopup = context.popups[context.popups.length - 1];
+    return {
+        popups: !lastPopup ? context.popups : context.popups.filter(popup => popup.id !== lastPopup.id)
+    };
+};
