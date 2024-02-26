8b23466092644edcf37e03c63717bd76f82001b7
dilaouid <dilaouid@student.42.fr>
2024-02-26 23:42:31 +0100

    (+) apply css depending on the grid size

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -7,8 +7,13 @@ type BoardProps = {
 };
 
 export const Board: React.FC<BoardProps> = ({ grid }) => {
+  const gridSize: string = import.meta.env.VITE_GRID_SIZE || '7';
+  const boardStyle = {
+    gridTemplateColumns: `repeat(${gridSize}, 1fr)`,
+    gridTemplateRows: `repeat(${gridSize}, 1fr)`,
+  };
   return (
-    <div className="board">
+    <div className="board" style={boardStyle}>
       {grid.map((row, rowIndex) =>
         row.map((type, columnIndex) => <Tile key={`${rowIndex}-${columnIndex}`} type={type} />)
       )}
