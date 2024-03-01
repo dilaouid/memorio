ed3172ce925fbed0271efd3c04438d0f8d8b110d
dilaouid <dilaouid@student.42.fr>
2024-03-01 18:38:02 +0100

    (~) update use-sound module

--- src/types/use-sound.d.ts
+++ src/types/use-sound.d.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module 'use-sound' {
     export default function useSound(
       path: string,
       options?: any
-    ): [() => void, { sound: any }];
+    ): [() => void, { sound: any, stop: () => void }];
 }
