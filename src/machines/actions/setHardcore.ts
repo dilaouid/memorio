d7d20a2d606f099f82747316a9387dadba001094
dilaouid <dilaouid@student.42.fr>
2024-03-03 20:38:44 +0100

    (+) save delay demo when switching in hardcore mode

--- src/machines/actions/setHardcore.ts
+++ src/machines/actions/setHardcore.ts
@@ -3,6 +3,7 @@ import { GameContext } from "../../types/Machine";
 export const setHardcoreAssign = (context: GameContext) => {
     return {
         isHardcoreMode: !context.isHardcoreMode,
+        savedDemoDelay: context.demoDelay,
         demoDelay: context.isHardcoreMode ? context.savedDemoDelay : context.hardCoreDelay
     };
 };
