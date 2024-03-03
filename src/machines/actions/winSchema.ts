ab4e9b3f53ca6f9fafc826fc4d8c1ccdfdd50d82
dilaouid <dilaouid@student.42.fr>
2024-03-03 20:39:35 +0100

    (+) adjust win actions with hardcore and slow mode

--- src/machines/actions/winSchema.ts
+++ src/machines/actions/winSchema.ts
@@ -3,8 +3,29 @@ import { ScorePopupProps } from "../../types/ScorePopupProps";
 import { calculateScore } from "../../utils/gameUtils";
 import { GameContext } from "../../types/Machine";
 
+const PATH_LENGTH_MAX = import.meta.env.VITE_DEFAUT_PATHLENGTH_MAX as number;
+const PATH_LENGTH_HARDCORE_MAX = import.meta.env.VITE_HARDCORE_PATHLENGTH_MAX as number;
+
 const newDifficulty = (context: GameContext) => {
-    return Math.min(context.pathLength + 1, import.meta.env.VITE_DEFAUT_PATHLENGTH_MAX as number)
+    const maxPathLength = context.isHardcoreMode ? PATH_LENGTH_HARDCORE_MAX : PATH_LENGTH_MAX;
+    return Math.min(context.pathLength + 1, maxPathLength as number)
+};
+
+const calculateDemoDelay = (context: GameContext, random: number) => {
+    const isHardcore = context.isHardcoreMode;
+    const isSlowMode = context.isSlowMode;
+
+    if (random < 0.3 && !isHardcore) {
+        return context.demoDelay;
+    }
+
+    if (isHardcore) {
+        return Math.max(context.hardCoreDelay, 60);
+    } else if (isSlowMode) {
+        return Math.max(context.demoDelay * 0.9, 500)
+    } else {
+        return Math.max(context.demoDelay * 0.9, 100);
+    }
 };
 
 export const winSchemaAssign = (context: GameContext) => {
@@ -25,7 +46,7 @@ export const winSchemaAssign = (context: GameContext) => {
         pathLength: random < 0.3 ? context.pathLength : newDifficulty(context),
 
         // demoDelay will MAYBE be decreased
-        demoDelay: random < 0.3 ? context.demoDelay : Math.max(context.demoDelay * 0.9, 100),
+        demoDelay: calculateDemoDelay(context, random),
         status: 'success' as LampStatus
     };
 };
